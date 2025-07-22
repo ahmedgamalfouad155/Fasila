3ddb4407786e3606dbe9a05d0ac3fdb31d96dc04
ahmedgfouad <ahmedgfouad2020@gmail.com>
2025-07-22 04:38:03 +0300

    refactor planet info section

--- lib/features/planet_details/presentation/view/widgets/planet_info_content.dart
+++ lib/features/planet_details/presentation/view/widgets/planet_info_content.dart
@@ -1,7 +1,7 @@
 import 'package:fasila/core/theme/colors.dart';
 import 'package:fasila/core/widgets/default_button_widget.dart';
 import 'package:fasila/features/category_details/data/models/planet_model.dart';
-import 'package:fasila/features/planet_details/presentation/view/widgets/planet_info_details_option_widget.dart';
+import 'package:fasila/features/planet_details/presentation/view/widgets/planet_info_section.dart';
 import 'package:flutter/material.dart';
 
 class PlanetInfoContent extends StatelessWidget {
@@ -17,7 +17,7 @@ class PlanetInfoContent extends StatelessWidget {
           onPressed: () {},
           icon: Icon(Icons.add, color: context.appColors.white),
         ),
-        PlanetInfoDetailsOptionWidget(planetModel: planetModel),
+        PlanetInfoSection(planetModel: planetModel),
       ],
     );
   }
---
+++ lib/features/planet_details/presentation/view/widgets/planet_info_section.dart
@@ -0,0 +1,27 @@
+import 'package:fasila/features/category_details/data/models/planet_model.dart';
+import 'package:fasila/features/planet_details/data/constant_planet_details.dart';
+import 'package:fasila/features/planet_details/presentation/manager/weeks_togel_cubit/days_toget_cubit.dart';
+import 'package:fasila/features/planet_details/presentation/view/widgets/empty_planet_info_widget.dart';
+import 'package:fasila/features/planet_details/presentation/view/widgets/planet_info_details_option_widget.dart';
+import 'package:fasila/features/shop/presentation/manager/filter_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class PlanetInfoSection extends StatelessWidget {
+  const PlanetInfoSection({super.key, required this.planetModel});
+  final PlanetModel planetModel;
+
+  @override
+  Widget build(BuildContext context) {
+    final weekIndex = context.watch<FilterCubit>().state;
+    final dayIndex = context.watch<DaysTogetCubit>().state;
+    if (planetModel.dailyFarm.isEmpty) {
+      return const EmptyPlanetInfoWidget();
+    } else {
+      final data = planetModel
+          .dailyFarm[weekIndex]
+          .days[ConstantPlanetDetails().days[dayIndex].toLowerCase()];
+      return PlanetInfoDetailsOptionWidget(data: data);
+    }
+  }
+}
